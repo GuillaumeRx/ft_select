706659e898054239ac1ab29065690e1ec8ed2161
Guillaume Roux <guillaumeroux123@gmail.com>
2019-10-08 18:08:16 +0200

    normed select.c

--- src/select.c
+++ src/select.c
@@ -6,59 +6,37 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 17:16:09 by guroux            #+#    #+#             */
-/*   Updated: 2019/10/08 17:57:41 by guroux           ###   ########.fr       */
+/*   Updated: 2019/10/08 18:07:54 by guroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-
-
 static int		handle_keypress(char buff[5], t_select **head)
 {
-	int			len;
-	int			arg_per_line;
-	struct winsize ws;
-	
+	int					len;
+	struct winsize		ws;
+
 	ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
 	len = calc_longest(*head);
-	arg_per_line = ws.ws_col / (len + 1);
-	if (verify_size(arg_per_line, args_number(*head), ws))
+	if (verify_size(ws.ws_col / (len + 1), args_number(*head), ws))
 	{
 		if (ft_strcmp(buff, tgetstr("kl", NULL)) == 0)
-		{
 			move_left(*head);
-			return (1);
-		}
-		if (ft_strcmp(buff, tgetstr("kr", NULL)) == 0)
-		{
+		else if (ft_strcmp(buff, tgetstr("kr", NULL)) == 0)
 			move_right(*head);
-			return (1);
-		}
-		if (ft_strcmp(buff, tgetstr("kd", NULL)) == 0)
-		{
-			move_down(*head, arg_per_line);
-			return (1);
-		}
-		if (ft_strcmp(buff, tgetstr("ku", NULL)) == 0)
-		{
-			move_up(*head, arg_per_line);
-			return (1);
-		}
-		if (ft_strcmp(buff, " ") == 0)
-		{
+		else if (ft_strcmp(buff, tgetstr("kd", NULL)) == 0)
+			move_down(*head, ws.ws_col / (len + 1));
+		else if (ft_strcmp(buff, tgetstr("ku", NULL)) == 0)
+			move_up(*head, ws.ws_col / (len + 1));
+		else if (ft_strcmp(buff, " ") == 0)
 			handle_select(*head);
-			return (1);
-		}
-		if ((buff[0] == 127 && buff[1] == 0) || ft_strcmp(buff, "\x1B[3~") == 0)
-		{
+		else if ((buff[0] == 127 && buff[1] == 0)
+		|| ft_strcmp(buff, "\x1B[3~") == 0)
 			remove_node(head);
-			return (1);
-		}
-		if (ft_strcmp(buff, "\e") == 0)
-		{
+		else if (ft_strcmp(buff, "\e") == 0)
 			return (-1);
-		}
+		return (1);
 	}
 	return (0);
 }
@@ -67,7 +45,7 @@ int				readterm(t_select **head)
 {
 	char	buff[4 + 1];
 	int		ret;
-	
+
 	if (head && *head)
 		handle_print(*head);
 	while ((ret = read(STDIN_FILENO, buff, 4)))
@@ -78,11 +56,9 @@ int				readterm(t_select **head)
 		{
 			tputs(tgetstr("cl", NULL), 2, ft_putcher);
 			if (head && *head)
-			{
 				handle_print(*head);
-			}
 			else
-				break;
+				break ;
 		}
 		else if (ret == -1)
 			return (0);
